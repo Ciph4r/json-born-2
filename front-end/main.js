16abee278a9dcae2b0d37d27ee09487629c79112
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-04-28 10:56:31 -0400

    'updated'

--- front-end/main.js
+++ front-end/main.js
@@ -1,1 +1,33 @@
-getUsers();
+// getUsers();
+
+const getUser = (id) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('loadend', (event) => {
+      console.log(event.target.response);
+    })
+  
+    xhr.open('GET',`http://localhost:3000/users/${id}`);
+    xhr.send();
+}
+
+// getUser(2)
+
+
+const postFriends = (friends ,id) => {
+  const xhr = new XMLHttpRequest()
+  const newFriend = {
+    friends,
+  }
+
+  xhr.addEventListener('loadend' , (event) => {
+    console.log(event.target.response);
+  })
+  xhr.open('POST' , `http://localhost:3000/users/1`)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.send(JSON.stringify(newFriend))
+}
+
+
+postFriends('james' , 1)
+
+// xhr.open('GET',`http://localhost:3000/users/${id}`);
